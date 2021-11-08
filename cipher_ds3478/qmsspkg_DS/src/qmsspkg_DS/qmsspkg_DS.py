2f8f558c3a0cab371fec30982c9d8e6aa2b9dd08
ds3478 <90806605+ds3478@users.noreply.github.com>
2021-11-07 23:55:13 -0500

    initial package setup
    initial package setup

--- cipher_ds3478/qmsspkg_DS/src/qmsspkg_DS/qmsspkg_DS.py
+++ cipher_ds3478/qmsspkg_DS/src/qmsspkg_DS/qmsspkg_DS.py
@@ -1,6 +1,38 @@
 def cipher(text:str, shift:int, encrypt:bool = True) -> str:
-    '''
-    '''
+    """
+    Encrypts or decrypts supplied text. Each letter is replaced by a letter
+    some fixed number of positions down the alphabet. Apparently, Julius Caesar
+    used it in his private correspondence.
+    
+    Parameters
+    ----------
+    text: str
+        the string to be encrypted or decrypted
+    shift: int
+        parameterizes the encryption / decryption
+    encrypt: bool, optional
+        if true then encrypt; otherwise descrypt. Default value is True
+
+    Returns
+    -------
+    str
+        a string that is encrypted or decrypted
+        
+    Raises
+    ------
+    TypeError
+        when 'shift' parameter is not an int
+        
+    Examples
+    --------
+    >>> from qmsspkg_DS import qmsspkg_DS
+    >>> cipher(text = 'absent', shift = 1, encrypt = True)
+    bctfou
+    >>> cipher(text = 'absent', shift = 10, encrypt = True)
+    klCoxD
+    >>> cipher(text = 'klCoxD', shift = 10, encrypt = False)
+    absent
+    """
     # https://docs.python.org/3/library/exceptions.html#TypeError
     if not type(shift) is int:
         raise TypeError('expected type of int for ''shift'' parameter')
--- cipher_ds3478/qmsspkg_DS/tests/test_qmsspkg_DS.py
+++ cipher_ds3478/qmsspkg_DS/tests/test_qmsspkg_DS.py
@@ -1,1 +1,21 @@
 from qmsspkg_DS import qmsspkg_DS
+
+import pytest
+
+def test_cipher_single_word():
+    result = cipher(text = 'absent', shift = 1, encrypt = True)
+    assert(result) == 'bctfou', 'Should be ''bctfou'''
+
+@pytest.mark.parametrize("example, shift, expected", [
+    ('absent', 1, 'bctfou'),
+    ('a', 1, 'b'),
+    ('Absent', 1, 'Bctfou'),
+    ('ABSENT', 1, 'BCTFOU'),
+    ('absent@', 1, 'bctfou@'),
+    ('-absent@', 1, '-bctfou@'),
+    ('student was ABSENT', 1, 'tuvefou xbt BCTFOU'),
+    ('$', 1, '$')
+])
+def test_cipher_cases_looping(example, shift, expected):
+    result = cipher(text = example, shift = shift, encrypt = True)
+    assert result == expected
